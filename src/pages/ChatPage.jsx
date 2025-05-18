f542b53efd9e75899f0204febc5efc343c8ef23a
pasan <minjanaap.21@itfac.mrt.ac.lk>
2025-05-18 16:17:36 +0530

    Feat : Handle message status like 'sent', 'delivered', 'seen'2.0 - change meesgae status and display in real-time.

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -3,14 +3,14 @@ import WaveBackground from "../components/WaveBackground"
 import Navbar from "../components/Navbar"
 import { ChatSection } from "../components/ChatPageComponents/ChatSection"
 import { useEffect } from "react"
-import { UpdateDeliveredMessages } from "../api/firebase.service"
+import { updateDeliveredMessages } from "../api/firebase.service"
 
 export const ChatPage = ({user}) => {
     useEffect(()=> {
         if (!user) return;
-        
+
         const UpdateMessageStatus = async () => {
-            await UpdateDeliveredMessages(user.uid);
+            await updateDeliveredMessages(user.uid);
         }
 
         UpdateMessageStatus();
